0defac8d25b7115b78d362dd6c2a37dc6b4d63dd
zhougod1 <834506443@qq.com>
2018-12-22 17:05:13 +0800

    加入射线跟踪鼠标

--- js/three_font.js
+++ js/three_font.js
@@ -1,10 +1,9 @@
 
 function init() {
-    var width;
-    var height;
-    var scene;
-    var camera;
-    var renderer;
+
+  var raycaster;
+  var mouse;
+  var INTERSECTED;
 
     // input type='file' 取到的路径是经过隐藏的路径（流浪器安全策略*/fakepath/*/）所以需要通过file取
     function getObjectURL(file) { 
@@ -16,7 +15,7 @@ function init() {
       // };
 
 
-      // 2.将file转真是文档路径 
+      // 2.将file转真实文档路径 
       var url = null;  
       if (window.createObjcectURL != undefined) {  
           url = window.createOjcectURL(file);  
@@ -30,12 +29,34 @@ function init() {
 
     document.getElementById('selectFile').addEventListener('change',function(){
       const url = getObjectURL(this.files[0]);
-      circleMaterial.map = new THREE.TextureLoader().load(url);
+      var circleMaterial = new THREE.MeshBasicMaterial({
+        // map: new THREE.TextureLoader().load('http://imgsrc.baidu.com/imgad/pic/item/314e251f95cad1c85d6d7d31743e6709c83d51d6.jpg'
+        // map: new THREE.TextureLoader().load('https://zhougod1.github.io/images/panorama/demo/view/room.jpg'
+        // map: new THREE.TextureLoader().load('images/panorama/demo/view/room.jpg'
+        // ,function(){
+        //   document.getElementById('loading').style.visibility = 'hidden'
+        // })
+        map: new THREE.TextureLoader().load(url)
+  
+      });   
+  
+      var circle = new THREE.Mesh(boxGeometry, circleMaterial);
+      // circle.position.set(0,-50,-50)
+      scene.add(circle)
+      // circleMaterial.map = new THREE.TextureLoader().load(url);
     })
+
+    document.getElementById('container').addEventListener('mousemove', function(e){
+      mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+      mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+    },false);
 
 
     // 场景设置（创建场景，设置渲染器，设置摄像机，设计灯光）
     function setScene(){
+
+      raycaster = new THREE.Raycaster();
+      mouse = new THREE.Vector2();
 
       // 获取浏览器窗口的宽高，后续会用
       width = window.innerWidth - 40;
@@ -45,7 +66,7 @@ function init() {
       scene = new THREE.Scene()
 
       // 创建一个具有透视效果的摄像机
-      camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000)
+      camera = new THREE.PerspectiveCamera(45, 1, 1, 1000)
 
       // 设置摄像机位置，并将其朝向场景中心
       camera.position.set(0, 125, 125)
@@ -58,11 +79,11 @@ function init() {
       // renderer = new THREE.CSS3DRenderer();
 
       // 设置渲染器的清除颜色（即背景色）和尺寸
-      // renderer.setClearColor(0xffffff)
-      renderer.setSize(width, height)
+      renderer.setClearColor(0xffffff)
+      renderer.setSize(height, height)
 
       // 将渲染器的输出（此处是 canvas 元素）插入到 body
-      document.body.appendChild(renderer.domElement)
+      document.getElementById('container').appendChild(renderer.domElement)
 
       // 创建 环境光
       var ambientLight = new THREE.AmbientLight(0x777777)
@@ -78,6 +99,23 @@ function init() {
     function render() {
       // 渲染，即摄像机拍下此刻的场景
       renderer.render(scene, camera)
+
+      // 通过摄像机和鼠标位置更新射线
+      raycaster.setFromCamera( mouse, camera );
+
+      // 计算物体和射线的焦点
+      var intersects = raycaster.intersectObjects( scene.children );
+      if ( intersects.length > 0 ) {
+        if ( INTERSECTED != intersects[ 0 ].object ) {
+          if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+          INTERSECTED = intersects[ 0 ].object;
+          INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+          INTERSECTED.material.color.setHex( 0x00ff00 );
+        }
+      } else {
+        if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+        INTERSECTED = null;
+      }
 
       // requestAnimationFrame在渲染完成后会执行函数
       // 做了一个递归，requestAnimationFrame会根据浏览器完成最高渲染次数 (chrome 是 60/s) ，
@@ -95,54 +133,61 @@ function init() {
 
 
 
-		var boxGeometry = new THREE.SphereGeometry(width/2,100,100)
+		var boxGeometry = new THREE.SphereGeometry(height/2,50,50)
 
     boxGeometry.scale(-1,1,1);
 		// var edgesGeometry = new THREE.SphereBufferGeometry(boxGeometry)
-		var circleMaterial = new THREE.MeshBasicMaterial({
+		// var circleMaterial = new THREE.MeshBasicMaterial({
 			// map: new THREE.TextureLoader().load('http://imgsrc.baidu.com/imgad/pic/item/314e251f95cad1c85d6d7d31743e6709c83d51d6.jpg'
       // map: new THREE.TextureLoader().load('https://zhougod1.github.io/images/panorama/demo/view/room.jpg'
       // map: new THREE.TextureLoader().load('images/panorama/demo/view/room.jpg'
       // ,function(){
       //   document.getElementById('loading').style.visibility = 'hidden'
       // })
+      // map: new THREE.TextureLoader().load()
 
-		});
+		// });
 
-    var circle = new THREE.Mesh(boxGeometry, circleMaterial);
+    // var circle = new THREE.Mesh(boxGeometry, circleMaterial);
 		// circle.position.set(0,-50,-50)
-		scene.add(circle)
+    // scene.add(circle)
+    
+
+
+
+
+
 
     // 平面
-    // // 创建一个平面 PlaneGeometry(width, height, widthSegments, heightSegments)
-    // var planeGeometry = new THREE.PlaneGeometry(120, 90, 1, 1)
-    // // 创建 Lambert 材质：会对场景中的光源作出反应，但表现为暗淡，而不光亮。
-    // var planeMaterial = new THREE.MeshLambertMaterial({
-    //   color: 0xffffff
-    // })
-    // var plane = new THREE.Mesh(planeGeometry, planeMaterial)
-    // // 以自身中心为旋转轴，绕 x 轴顺时针旋转 45 度
-    // plane.rotation.x = -0.5 * Math.PI
-    // plane.position.set(0, -10.5, -20)
-    // scene.add(plane)
+    // 创建一个平面 PlaneGeometry(width, height, widthSegments, heightSegments)
+    var planeGeometry = new THREE.PlaneGeometry(120, 90, 1, 1)
+    // 创建 Lambert 材质：会对场景中的光源作出反应，但表现为暗淡，而不光亮。
+    var planeMaterial = new THREE.MeshLambertMaterial({
+      color: 0xffffff
+    })
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial)
+    // 以自身中心为旋转轴，绕 x 轴顺时针旋转 45 度
+    plane.rotation.x = -0.5 * Math.PI
+    plane.position.set(0, -10.5, -20)
+    scene.add(plane)
 
 
     // 创建红色的线材质
-    // var lineMaterial = new THREE.LineBasicMaterial({
-    //   color: 0xff0000
-    // })
+    var lineMaterial = new THREE.LineBasicMaterial({
+      color: 0xff0000
+    })
 
     //  创建一个半径为 8 个球体
-    // var boxGeometry = new THREE.SphereGeometry(8)
-    // // This can be used as a helper object to view the edges of a Geometry object
-    // var edgesGeometry = new THREE.EdgesGeometry(boxGeometry)
-    // var edgesLine = new THREE.LineSegments(edgesGeometry, lineMaterial)
-    // edgesLine.position.x = -30
-    // scene.add(edgesLine)
-    // // This can be used as a helper object to view a Geometry object as a wireframe.
-    // var wrieframe = new THREE.WireframeGeometry(boxGeometry)
-    // var wrieframeLine = new THREE.LineSegments(wrieframe, lineMaterial)
-    // scene.add(wrieframeLine)
+    var boxGeometry = new THREE.SphereGeometry(8)
+    // This can be used as a helper object to view the edges of a Geometry object
+    var edgesGeometry = new THREE.EdgesGeometry(boxGeometry)
+    var edgesLine = new THREE.LineSegments(edgesGeometry, lineMaterial)
+    edgesLine.position.x = -30
+    scene.add(edgesLine)
+    // This can be used as a helper object to view a Geometry object as a wireframe.
+    var wrieframe = new THREE.WireframeGeometry(boxGeometry)
+    var wrieframeLine = new THREE.LineSegments(wrieframe, lineMaterial)
+    scene.add(wrieframeLine)
 
     // 创建一个二维形状：三角形
     // function drawShape() {
@@ -155,6 +200,9 @@ function init() {
     //  return the shape
     //   return shape;
     // }
+
+
+
     // function loadTexture(d){
     //   var b = new THREE.Texture()
     //       b.image = new THREE.TextureLoader().load(d);
@@ -164,6 +212,7 @@ function init() {
     //   });
     //   return a
     // }
+
     // var index = 0;
     // var b = "mobile";
     // var a = [loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_f.jpg"), loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_b.jpg"), loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_u.jpg"), loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_d.jpg"), loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_l.jpg"), loadTexture(load_url + "/" + villaId + "/" + name_arr[index] + "/" + b + "_r.jpg")];
@@ -247,13 +296,14 @@ function init() {
     //   curveSegments: 12,
     //   steps: 1
     // }
+
     // 将一个二维形状按照指定参数向 Z 轴拉伸
     // var extrudeGeometry = new THREE.ExtrudeGeometry(drawShape(), options)
     // var mesh = new THREE.Mesh(extrudeGeometry, meshMaterial)
-
     // mesh.position.x = 20
     // scene.add(mesh)
     // 创建一个字体加载器
+
     // var fontLoader = new THREE.FontLoader()
 
     // 字体的本质就是一堆类似 SVG 的路径，因此直线占比越高越好。
